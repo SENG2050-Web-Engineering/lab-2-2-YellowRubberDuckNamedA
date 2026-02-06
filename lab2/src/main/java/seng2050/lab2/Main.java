9adc799d6f02b25a745c88a4b08babcaf9656238
YellowRubberDuckNamedA <c3536131@uon.edu.au>
2026-02-06 13:40:51 +1100

    greeetingpoush

--- lab2/src/main/java/seng2050/lab2/Main.java
+++ lab2/src/main/java/seng2050/lab2/Main.java
@@ -42,6 +42,10 @@ public class Main {
     tomcat.addServlet("", "SimpleMessage", new SimpleMessage());
     ctx.addServletMappingDecoded("/SimpleMessage", "SimpleMessage");
 
+// Add and map the servlet
+    tomcat.addServlet("", "GreetingsServlet", new GreetingsServlet());
+    ctx.addServletMappingDecoded("/GreetingsServlet", "GreetingsServlet");
+
     // Add and map the servlet
     tomcat.addServlet("", "generateValidHTML", new generateValidHTML());
     ctx.addServletMappingDecoded("/generateValidHTML", "generateValidHTML");
